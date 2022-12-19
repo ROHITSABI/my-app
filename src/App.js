0444a804fdf730c9ae27902f00d32728255f2153
Rohit <rsabi2708@gmail.com>
2022-12-19 15:00:35 +0530

    multi-tag

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <h1>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis repellat
+      quod enim necessitatibus incidunt, explicabo earum sunt reiciendis illo
+      delectus voluptatibus esse veritatis fugit, numquam alias nemo minus amet
+      ex!
+    </h1>
+  );
 }
 
 export default App;
