1ec549d04de1d8d4e99987a1357397501fb253b2
Rohit <rsabi2708@gmail.com>
2022-12-19 17:30:09 +0530

    member function

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ function App() {
   let project = "Counter Application";
   let btnValue = "Click Me";
 
+  let clickMe = () => alert();
+  let increment = () => {};
+
   return (
     <div>
       <h1>{project}</h1>
-      <input type="button" value="{btnValue}" />
+      <input type="button" value={btnValue} onClick={clickMe} />
+      <input type="button" value="Increment" onClick={increment} />
     </div>
   );
 }
