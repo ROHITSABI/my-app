11379140cc713e6217634cc9df64d65a6e7180b4
Rohit <rsabi2708@gmail.com>
2022-12-19 22:44:51 +0530

    stateful variable

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import { useState } from "react";
 
 function App() {
-  let str1 = "Home";
-  let str2 = "Explore";
-  let str3 = "Notification";
-
   let [counter, setCounter] = useState(50);
   let increment = () => {
     counter++;
@@ -13,21 +9,9 @@ function App() {
     setCounter(counter);
   };
 
-  let [city, setCity] = useState("Bangalore");
-  let changeCity = () => {
-    city = "Hello" + city;
-    setCity(city);
-  };
-
   return (
     <div>
-      <h1>{city}</h1>
-      <input type="button" value="Change City" onClick={changeCity} />
-
-      <hr />
-      <h1>{str1}</h1>
-      <h1>{str2}</h1>
-      <h1>{str3}</h1>
+      <h1>Counter Application</h1>
       <h1>{counter}</h1>
       <input type="button" value="Increment" onClick={increment} />
     </div>
