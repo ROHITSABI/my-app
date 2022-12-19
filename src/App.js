85efa37a9b54da5a926f7f6b3581c2c6e0a312a1
Rohit <rsabi2708@gmail.com>
2022-12-19 15:16:28 +0530

    multiple-jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Person />
+      <Person />
+      <Person />
+      <Person />
     </div>
   );
 }
-
+function Person() {
+  return (
+    <div>
+      <img src="https://picsum.photos/200" alt="" />
+      <h1>Rohit</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum,
+        libero pariatur asperiores possimus obcaecati odio consectetur numquam
+        accusantium sit totam enim eaque assumenda rem placeat quis deserunt
+        exercitationem quasi quas?
+      </p>
+    </div>
+  );
+}
 export default App;
