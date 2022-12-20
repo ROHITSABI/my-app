c963d76d208ac962ae690c69aa665db80e77e29d
Rohit <rsabi2708@gmail.com>
2022-12-20 16:16:28 +0530

    dynamic css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
+import { useState } from "react";
+
 function App() {
+  let [theme, setTheme] = useState("Primary");
+  let primaryTheme = () => {
+    theme = "primary";
+
+    setTheme(theme);
+  };
+
+  let successTheme = () => {
+    theme = "success";
+
+    setTheme(theme);
+  };
+
+  let dangerTheme = () => {
+    theme = "danger";
+
+    setTheme(theme);
+  };
   return (
     <div>
-      <h1 className="bg-dark text-light p-3">BootStrap</h1>
-      <h1 className="bg-success" style={{ color: "#fff" }}>
-        Hello
-      </h1>
+      <h1 className={`bg-${theme} text-dark p-3`}>BootStrap Styling</h1>
+      <input
+        className="btn btn-primary"
+        type="button"
+        value="Primary Theme"
+        onClick={primaryTheme}
+      />
+      <input
+        className="btn btn-success"
+        type="button"
+        value="Success Theme"
+        onClick={successTheme}
+      />
+      <input
+        className="btn btn-danger"
+        type="button"
+        value="Danger Theme"
+        onClick={dangerTheme}
+      />
     </div>
   );
 }
