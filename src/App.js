100911670bd37071f15bd3bf40f12b8c954d5a0c
Rohit <rsabi2708@gmail.com>
2022-12-20 17:18:05 +0530

    dynamic css using map

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { useState } from "react";
 
 function App() {
+  let [list] = useState([1, 1, 1, 1]);
   let [theme, setTheme] = useState("Primary");
+
   let primaryTheme = () => {
     theme = "primary";
 
@@ -21,25 +23,36 @@ function App() {
   };
   return (
     <div>
-      <h1 className={`bg-${theme} text-dark p-3`}>BootStrap Styling</h1>
-      <input
-        className="btn btn-primary"
-        type="button"
-        value="Primary Theme"
-        onClick={primaryTheme}
-      />
-      <input
-        className="btn btn-success"
-        type="button"
-        value="Success Theme"
-        onClick={successTheme}
-      />
-      <input
-        className="btn btn-danger"
-        type="button"
-        value="Danger Theme"
-        onClick={dangerTheme}
-      />
+      <div className="sticky-top">
+        <h1 className={`bg-${theme} text-dark p-3`}>BootStrap Styling</h1>
+        <input
+          className="btn btn-primary"
+          type="button"
+          value="Primary Theme"
+          onClick={primaryTheme}
+        />
+        <input
+          className="btn btn-success"
+          type="button"
+          value="Success Theme"
+          onClick={successTheme}
+        />
+        <input
+          className="btn btn-danger"
+          type="button"
+          value="Danger Theme"
+          onClick={dangerTheme}
+        />
+      </div>
+
+      {list.map((item) => (
+        <div className={`alert alert-${theme} my-1`}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+          unde dolore corporis enim nobis nostrum officiis ab obcaecati? Odit
+          dolorem soluta commodi, aperiam repellat dolores sunt numquam? Ut,
+          ipsa aspernatur!
+        </div>
+      ))}
     </div>
   );
 }
