a781fe5b1b85b3459ce9ba235327b15f4fef48f6
Rohit <rsabi2708@gmail.com>
2022-12-20 14:45:04 +0530

    external styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import "./App.css";
-
 function App() {
-  let myStyle = { color: "red", background: "purple" };
-
   return (
     <div>
-      <h1 className="beautify">Hello Universe</h1>
-      <h1 style={myStyle}>Hello World</h1>
-      <h1 style={{ color: "white", background: "red" }}>Hello</h1>
+      <h1 className="beautify">External Styling</h1>
     </div>
   );
 }
