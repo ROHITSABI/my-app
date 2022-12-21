1a3afb8bcc5c3e25e0b620e4a05d864318701e96
Rohit <rsabi2708@gmail.com>
2022-12-21 15:20:59 +0530

    capture the input

--- src/App.js
+++ src/App.js
@@ -1,34 +1,23 @@
+import { useState } from "react";
+
 function App() {
-  let clickMe1 = () => {
-    console.log("Hello World");
+  let [message, setMessage] = useState("abc");
+  let updateMessage = (e) => {
+    message = e.target.value;
+
+    setMessage(message);
   };
 
-  let clickMe2 = (e) => {
-    console.log(e);
-  };
-
-  let clickMe3 = (p1) => {
-    console.log(p1);
-  };
-
-  let clickMe4 = (e, p1) => {
-    console.log(e, p1);
-  };
   return (
     <div>
-      <h1>Btn Click Demo</h1>
-      <input type="button" value="Click Me1" onClick={clickMe1} />
-      <input type="button" value="Click Me2" onClick={clickMe2} />
+      <h1>Working With Input</h1>
       <input
-        type="button"
-        value="Click Me3"
-        onClick={() => clickMe3("primary")}
+        type="text"
+        placeholder="Enter Message"
+        value={message}
+        onChange={updateMessage}
       />
-      <input
-        type="button"
-        value="Click Me4"
-        onClick={(e) => clickMe4(e, "hello moto")}
-      />
+      <h1>{message}</h1>
     </div>
   );
 }
