772fd37c73d6b76a82b0cf0ef79ab036dec8ec7e
Rohit <rsabi2708@gmail.com>
2022-12-21 16:30:30 +0530

    messaging

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import { useState } from "react";
 
 function App() {
-  let [list] = useState(["delhi", "bangalore", "pune"]);
+  let [list] = useState([
+    { message: "Hi", messageTime: new Date() },
+    { message: "How r u", messageTime: new Date() },
+    { message: "I'm Good", messageTime: new Date() },
+    { message: "Thanku", messageTime: new Date() },
+  ]);
 
   return (
     <div>
-      <h1>Map Demo</h1>
-      {list.map((item) => (
-        <div className="alert alert-primary">{item}</div>
+      <h1 className="bg-primary text-white p-3">Messaging Demo</h1>
+
+      {list.map((item, index) => (
+        <div key={index} className="d-flex my-1">
+          <div className="badge text-bg-primary">
+            {item.message}
+            <span className="ms-4">
+              {item.messageTime.getHours()}:{item.messageTime.getMinutes()}
+            </span>
+          </div>
+        </div>
       ))}
     </div>
   );
